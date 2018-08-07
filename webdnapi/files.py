037a8e8ff7a5ceb7d850bf389c431c8984f7d8f5
David Darling <ddarling29@gmail.com>
2018-08-07 17:25:40 -0500

    Added support for Input Files Updated License
    The load API call simply returns a key, value dictionary with all the provided settings and their values.

--- webdnapi/files.py
+++ webdnapi/files.py
@@ -14,8 +14,18 @@ def load_project_settings():
 class ProjectSettings:
     def __init__(self, dictionary, file_string):
         self.file_string = file_string
-        self.name = dictionary['name']
+        self.name = dictionary.get('name', None)
         self.generation = utils.Generation(dictionary=dictionary['generation'])
         self.script_chain = dictionary['script_chain']
         self.execution_time = dictionary['execution_time']
+
+def load_input():
+    input_settings = {}
+    with open('input.txt', 'r') as input:
+        for line in input:
+            (key, val) = line.split(' = ')
+            input_settings[key] = val
+
+    return input_settings
+
 
