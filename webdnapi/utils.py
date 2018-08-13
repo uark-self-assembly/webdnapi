a5315db784375883a405d7e4059814a79e52416e
David Darling <ddarling29@gmail.com>
2018-08-12 20:38:40 -0500

    Added Type Conversions for Nucleotide and Configuration Classes
    This type conversion makes more sense and ensure there aren't any newlines read in from the files.

--- webdnapi/utils.py
+++ webdnapi/utils.py
@@ -46,13 +46,13 @@ class Configuration:
                  box_length_x=None, box_length_y=None, box_length_z=None,
                  energy_total=None, energy_potential=None, energy_kinetic=None,
                  nucleotides=None):
-        self.time_step = time_step
-        self.box_length_x = box_length_x
-        self.box_length_y = box_length_y
-        self.box_length_z = box_length_z
-        self.energy_total = energy_total
-        self.energy_potential = energy_potential
-        self.energy_kinetic = energy_kinetic
+        self.time_step = int(time_step)
+        self.box_length_x = float(box_length_x)
+        self.box_length_y = float(box_length_y)
+        self.box_length_z = float(box_length_z)
+        self.energy_total = float(energy_total)
+        self.energy_potential = float(energy_potential)
+        self.energy_kinetic = float(energy_kinetic)
         if nucleotides is None:
             self.nucleotides = []
         else:
@@ -65,18 +65,18 @@ class Nucleotide:
                  normal_v_x=None, normal_v_y=None, normal_v_z=None,
                  velocity_x=None, velocity_y=None, velocity_z=None,
                  ang_velocity_x=None, ang_velocity_y=None, ang_velocity_z=None):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.pos_z = pos_z
-        self.base_v_x = base_v_x
-        self.base_v_y = base_v_y
-        self.base_v_z = base_v_z
-        self.normal_v_x = normal_v_x
-        self.normal_v_y = normal_v_y
-        self.normal_v_z = normal_v_z
-        self.velocity_x = velocity_x
-        self.velocity_y = velocity_y
-        self.velocity_z = velocity_z
-        self.ang_velocity_x = ang_velocity_x
-        self.ang_velocity_y = ang_velocity_y
-        self.ang_velocity_z = ang_velocity_z
+        self.pos_x = float(pos_x)
+        self.pos_y = float(pos_y)
+        self.pos_z = float(pos_z)
+        self.base_v_x = float(base_v_x)
+        self.base_v_y = float(base_v_y)
+        self.base_v_z = float(base_v_z)
+        self.normal_v_x = float(normal_v_x)
+        self.normal_v_y = float(normal_v_y)
+        self.normal_v_z = float(normal_v_z)
+        self.velocity_x = float(velocity_x)
+        self.velocity_y = float(velocity_y)
+        self.velocity_z = float(velocity_z)
+        self.ang_velocity_x = float(ang_velocity_x)
+        self.ang_velocity_y = float(ang_velocity_y)
+        self.ang_velocity_z = float(ang_velocity_z)
